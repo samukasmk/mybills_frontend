7771d01961ba106b74c8f7dfae3f257f2935ccd6
Samuel Sampaio <samukasmk@gmail.com>
2020-08-17 22:07:45 -0300

    Initial Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Dashboard from './components/Dashboard/Dashboard.js'
 
 function App() {
   return (
     <div className="App">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+class Dashboard extends React.Component {
+  render() {
+    return <Box><span>Dashboard Component</span></Box>;
+  }
+}
+
+export default Dashboard;
